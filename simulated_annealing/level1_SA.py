73a2d07ae349d637a5ebe4aaa51b6815f695ff51
tejalbarnwal <tejalbarnwal@gmail.com>
2023-03-20 17:09:24 +0530

    refactor: level1 and level2 problem scripts

---
+++ simulated_annealing/level1_SA.py
@@ -0,0 +1,202 @@
+import numpy as np
+import random
+import pandas as pd
+import math
+from matplotlib import pyplot as plt
+
+
+class Optimization:
+    def __init__(self, df):
+        KM_M_CONVERSION = 1000
+        self.x_vector = df['X coord'].to_numpy().reshape(1, 10) * KM_M_CONVERSION
+        self.y_vector = df['Y coord'].to_numpy().reshape(1, 10) * KM_M_CONVERSION
+
+        self.speed = 5 #m/s
+
+        self.travel_time_matrix = self.generate_travel_time_matrix(self.x_vector, self.y_vector, self.speed)
+        
+        # memory of results
+        self.paths_visited_history = []
+        self.objective_value_history = []
+        self.tempertaure_history = []
+        
+        
+    def generate_travel_time_matrix(self, x, y, speed):
+        coordinates = np.concatenate((x, y), axis=0)
+        # print("c:", coordinates.shape)
+        a = coordinates.transpose()
+        b = a.reshape(np.prod(a.shape[:-1]), 1, a.shape[-1])
+        D = np.sqrt(np.einsum('ijk,ijk->ij',  b - a,  b - a)).squeeze()   
+        return D/speed
+            
+            
+    def objective_function(self, new_path, travel_time_matrix):
+        total_travel_time = 0.0
+        for i in range(len(new_path)-1):
+            # print(i)
+            total_travel_time += travel_time_matrix[new_path[i], new_path[i+1]] 
+        return total_travel_time
+    
+    
+    def create_new_path(self, new_path):
+        # CONSIDERING 0 AS DEPOT
+        N_CITIES = len(new_path) - 1
+        # random.seed(0)
+        positions_to_swap = random.sample(range(1, N_CITIES), 2) 
+        temp = new_path[positions_to_swap[0]]
+        new_path[positions_to_swap[0]] = new_path[positions_to_swap[1]]
+        new_path[positions_to_swap[1]] = temp
+        
+        return new_path
+    
+    def create_new_path1(self, new_path, seed_):
+        # CONSIDERING 0 AS DEPOT
+        N_CITIES = len(new_path) - 2
+        random.seed(seed_)
+        position = random.sample(range(1, N_CITIES), 1) 
+        positions_to_swap = [position[0], position[0]+1]
+        temp = new_path[positions_to_swap[0]]
+        new_path[positions_to_swap[0]] = new_path[positions_to_swap[1]]
+        new_path[positions_to_swap[1]] = temp
+        
+        return new_path
+    
+    
+    def store_visited_paths(self, path):
+        self.paths_visited_history.append(path.copy())
+#         print("paths visited:", self.paths_visited_history)
+#         print("type paths visited:", type(self.paths_visited_history))
+        
+    
+    def store_results(self, objective_value, temperature):
+        self.objective_value_history.append(objective_value)
+        self.tempertaure_history.append(temperature)
+    
+
+    def simulated_annealing(self, n_iterations, temperature, initial_guess):
+        COOLING_RATE = 0.999
+        CONSTANT_C = 1
+        # N_CITIES = len(initial_guess) - 1
+        
+        # calculate a feasible initial path
+        current_path = initial_guess.copy()
+        
+        # calculate objective function for obtained initial feasible path
+        current_objective_value = self.objective_function(current_path, 
+                                                             self.travel_time_matrix)
+        print("current_objective_function:", current_objective_value)
+        
+        current_temperature = temperature
+        
+        self.store_visited_paths(current_path)
+        self.store_results(current_objective_value, current_temperature)
+        
+        new_path = current_path.copy()
+        print("first new path:", new_path, " type:", type(new_path))
+        print("type paths visited:", type(self.paths_visited_history))
+        
+        print("##############################################################")
+        
+        j = 0
+        
+        for i in range(n_iterations):
+            new_path = self.create_new_path1(current_path.copy(), i)
+            
+            print("feasible new path:", new_path)
+            # print("current path:", current_path) 
+            
+            # calculate objective function for obtained initial feasible path
+            new_objective_value = self.objective_function(new_path, 
+                                                                 self.travel_time_matrix)
+            
+            # compare with the current path if the new one is better
+            if new_objective_value <= current_objective_value:
+                print("condition 1: move ahead")
+                current_path = new_path.copy()
+                current_objective_value = new_objective_value
+                
+                # current_temperature = temperature
+                # j = 0
+                
+            elif new_objective_value > current_objective_value:
+                print("__find probability__")
+                # accept the solution with some proabablity
+                diff_obj_value = (new_objective_value-current_objective_value)
+                probability = np.exp(- CONSTANT_C * diff_obj_value / current_temperature)
+                print("probability: ", probability)
+
+                if probability >= random.uniform(0, 1):
+                    print("condition 2: accepting solution with probabity")
+                    current_path = new_path.copy()
+                    current_objective_value = new_objective_value
+                     
+                else:
+                    pass  
+                
+                # print("2 cond=>j:", j)
+                # current_temperature = temperature * pow(COOLING_RATE, j)
+            else:
+                pass
+                # print("3 cond=>j: ")
+                # current_temperature = temperature * pow(COOLING_RATE, j)
+            # update temperature
+            # j = j + 1
+
+            current_temperature = temperature * np.power(COOLING_RATE, i)
+            
+            print("---------------")
+            print("iteration number: ", i)
+            print("current path: ", current_path)
+            print("current objective value: ", current_objective_value)
+            print("current_temperature: ", current_temperature, "\n")
+            print("-------------------------------------------")
+            self.store_visited_paths(current_path)
+            self.store_results(current_objective_value, current_temperature)
+            
+            
+        return current_path    
+
+
+
+file = '/home/radiant/Acads/ae755_project/project_venv/optimization-project/datasets/level2_dataset - Sheet2.csv'
+df = pd.read_csv(file)  
+
+obj = Optimization(df)
+initial_guess = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+n_iterations = 10000
+temperature = 10000
+solution = obj.simulated_annealing(n_iterations, temperature, initial_guess)
+
+
+n = len(obj.objective_value_history)
+a = np.arange(0, n)
+
+# for i in range(n):
+#     plt.subplot(2, 1, 1)
+#     plt.plot(i, obj.tempertaure_history[i], "c.")
+#     plt.title("temp vs iterations")
+#     plt.subplot(2, 1, 2)
+#     plt.plot(i, obj.objective_value_history[i], "g.")
+#     plt.title("objective value vs iterations")
+#     plt.pause(0.00000001)
+
+plt.subplot(2, 1, 1)
+plt.plot(a, obj.tempertaure_history, "k-")
+plt.subplot(2, 1, 2)
+plt.plot(a, obj.objective_value_history, "k-")
+# plt.pause(0.0001)
+
+plt.figure("2")
+plt.scatter(obj.x_vector, obj.y_vector)
+for i in range(len(solution)-1):
+    j = solution[i]
+    j_ = solution[i+1]
+    x_list = [obj.x_vector[0, j], obj.x_vector[0, j_]]
+    y_list = [obj.y_vector[0, j], obj.y_vector[0, j_]]
+    # print(x_list, y_list)
+    plt.plot(x_list, y_list)
+
+
+
+
+plt.show()
---
+++ simulated_annealing/ratrigen_SA.py
@@ -0,0 +1,159 @@
+import numpy as np
+import random
+import pandas as pd
+import math
+from matplotlib import pyplot as plt
+import time
+
+
+class Optimization:
+    def __init__(self):
+        self.history_obj = np.array([])
+        self.history_temp = np.array([])
+        self.history_x = np.array([])
+
+    # HIGHLIGHT
+    def objective_function(self, x):
+        f = 10 + np.power(x, 2) - (10 * np.cos(2 * (np.pi) * x))
+        return f
+
+    # HIGHTLIGHT
+    def find_neighbour(self, x, i):
+        random.seed(i)
+        x_new = x + random.gauss(0, 1)
+        while (x_new < -3) or (x_new > 3):
+            x_new = x + random.gauss(0, 1)
+        return x_new
+
+    def store_results(self, x, objective_value, temperature):
+        self.history_obj = np.append(self.history_obj, objective_value)
+        self.history_temp = np.append(self.history_temp, temperature)
+        self.history_x = np.append(self.history_x, x)
+
+    def simulated_annealing(self, n_iterations, temperature, initial_guess, cooling_rate):
+        # HIGHLIGHT
+        # TUNING PARAMS: n_iterations, temperature, cooling schedule
+        COOLING_RATE = cooling_rate
+        CONSTANT_C = 1
+
+        current_x = initial_guess
+        current_objective_value = self.objective_function(current_x)
+
+        print("Intial x:", current_x)
+        print("Initial objective value:", current_objective_value)
+
+        current_temperature = temperature
+
+        self.store_results(current_x, 
+                            current_objective_value, 
+                            current_temperature)
+        
+        new_x = current_x
+
+        for i in range(n_iterations):
+            new_x = self.find_neighbour(current_x, i)
+            new_objective_value = self.objective_function(new_x)
+
+            # HIGHLIGHT
+            if new_objective_value <= current_objective_value:
+                print("condition 1: move ahead")
+                current_x = new_x
+                current_objective_value = new_objective_value
+
+            elif new_objective_value > current_objective_value:
+                print("__find probability__")
+                diff_obj_value = (new_objective_value-current_objective_value)
+                prob = np.exp(- CONSTANT_C * diff_obj_value / current_temperature)
+                print("probability: ", prob)
+
+                if prob >= random.uniform(0, 1):
+                    print("condition 2: accepting solution with probabity")
+                    current_x = new_x
+                    current_objective_value = new_objective_value
+
+                else:
+                    pass
+            else:
+                pass
+
+            current_temperature = temperature * np.power(COOLING_RATE, i)
+
+            print("---------------")
+            print("iteration number: ", i)
+            print("current x: ", current_x)
+            print("current objective value: ", current_objective_value)
+            print("current_temperature: ", current_temperature, "\n")
+
+            self.store_results(current_x, 
+                            current_objective_value, 
+                            current_temperature)
+
+
+        return current_x
+
+
+
+obj = Optimization()
+
+## plot rastridge function 
+# x = np.arange(-3, 3, 0.1, dtype=float)
+# y = obj.objective_function(x)
+# print("y: ", y)
+# print("x:", x)
+# plt.plot(x, y)
+# plt.show()
+
+init_guess = 2.99
+n_iterations = 150
+temperature = 10000
+cooling_rate = 0.9
+
+obj.simulated_annealing(n_iterations, temperature, init_guess, cooling_rate)
+
+print("##########")
+
+# print("history x: ", obj.history_x)
+
+# print("history obj:", obj.history_obj)
+
+# print("history temp", obj.history_temp)
+
+n = len(obj.history_x)
+
+# time.sleep()
+
+plt.subplot(3, 1, 1)
+plt.plot(obj.history_x[0], obj.history_obj[0], "ro-")
+plt.subplot(3, 1, 2)
+plt.plot(0, obj.history_temp[0], "ro-")
+plt.subplot(3, 1, 3)
+plt.plot(0, obj.history_obj[0], "ro-")
+plt.pause(0.1)
+
+for i in range(n):
+    plt.subplot(3, 1, 1)
+    plt.plot(obj.history_x[i], obj.history_obj[i], "y.")
+    plt.title("Space x explored")
+    plt.subplot(3, 1, 2)
+    plt.plot(i, obj.history_temp[i], "c.")
+    plt.title("Temp V/S Iterations")
+    plt.subplot(3, 1, 3)
+    plt.plot(i, obj.history_obj[i], "g.")
+    plt.title("Objective value V/S Iterations")
+    plt.pause(0.1)
+
+plt.subplot(3, 1, 1)
+plt.plot(obj.history_x[n-1], obj.history_obj[n-1], "ko-")
+plt.subplot(3, 1, 2)
+plt.plot(n-1, obj.history_temp[n-1], "ko-")
+plt.subplot(3, 1, 3)
+plt.plot(n-1, obj.history_obj[n-1], "ko-")
+plt.pause(0.1)
+
+
+plt.show()
+
+
+
+
+
